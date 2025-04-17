8bbae86d087eb1c2bad2c4f559cfb75a3683f00b
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-17 07:39:34 +0700

    [BI-13][Server] Implement action of getting current user and checking authentication

--- src/lib/actions/auth.action.ts
+++ src/lib/actions/auth.action.ts
@@ -1,5 +1,6 @@
 "use server";
 import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
 
 const ONE_WEEK = 60 * 60 * 24 * 7; // 1 week || 60s, 60m, 24h, 7d
 
@@ -75,4 +76,30 @@ export const setSessionCookie = async (idToken: string) => {
     path: "/",
     sameSite: "lax",
   });
+};
+
+export const getCurrentUser = async (): Promise<User | null> => {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const isAuthenticated = async () => {
+  const user = await getCurrentUser();
+  return !!user; // true if have user, else false
 };
