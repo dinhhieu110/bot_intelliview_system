08f74d7d1616b8136670d9fe56013bcd730c87ed
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-17 07:40:08 +0700

    [BI-14][Client] Add protected pages

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,13 @@
 import Header from "@/src/components/layout/Header";
+import { isAuthenticated } from "@/src/lib/actions/auth.action";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
 type Props = {};
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
   return (
     <div>
       <div className="root-layout">
