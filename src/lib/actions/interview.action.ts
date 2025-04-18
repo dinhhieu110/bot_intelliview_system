519d64e3589baa17151a529609abf444733394b0
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-04-18 07:36:03 +0700

    [BI-19][Server] Implement getting list of other users' generated interviews

--- src/lib/actions/interview.action.ts
+++ src/lib/actions/interview.action.ts
@@ -1,12 +1,28 @@
 import { db } from "@/firebase/admin";
 
-export const getInterviewByUserUi = async (
+export const getInterviewsByUserId = async (
   userId: string
 ): Promise<Interview[] | null> => {
   const interviewRecords = await db
     .collection("interviews")
     .where("userId", "==", userId)
     .orderBy("createdAt", "desc")
+    .get();
+  return interviewRecords.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+};
+
+export const getLatestInterviews = async (
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> => {
+  const { userId, limit = 20 } = params;
+  const interviewRecords = await db
+    .collection("interviews")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId)
+    .limit(limit)
     .get();
   return interviewRecords.docs.map((doc) => ({
     id: doc.id,
